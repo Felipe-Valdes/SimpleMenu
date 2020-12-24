9bb8755963c3a9c12b7e6e4b157434b8bc377379
Felipe-Valdes <61599227+Felipe-Valdes@users.noreply.github.com>
2020-12-24 01:11:47 -0300

    Add files via upload
    Método main, está dispuesto a muchos cambios, y lo utilizamos principalmente para ir probando los demás métodos

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Opciones persona = new Opciones();
+        persona.bienvenida();
+        System.out.println("");
+        persona.ventana1();
+
+
+    }
+
+}
