f65b7e8563d48b14da9b3703db80e25788ce2db2
Felipe-Valdes <61599227+Felipe-Valdes@users.noreply.github.com>
2020-12-27 22:18:16 -0300

    Add files via upload
    Clases del código (primer avance)

---
+++ Código/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Producto crearProducto = new Producto();
+        Menus menu = new Menus();
+
+        menu.ventana1();
+
+
+    }
+
+}
