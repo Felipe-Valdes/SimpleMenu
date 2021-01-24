be54df302db3a9bd135ff2709d9f79c7de637090
Felipe-Valdes <61599227+Felipe-Valdes@users.noreply.github.com>
2021-01-24 17:26:50 -0300

    Update Metodo.java
    -Añadidos métodos para quitar productos
    -Actualizados los métodos en general

--- Código/Metodo.java
+++ Código/Metodo.java
@@ -1,38 +1,68 @@
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.io.IOException;
 
 
 public class Metodo {
-    Scanner teclado = new Scanner(System.in);
     public static int nCompletos;
     public static int nSopaipillas;
     public static int nPanesConQueso;
 
+    //completos
 
-    public int añadirCompletos(int nCompletos) {
-        this.nCompletos = nCompletos + this.nCompletos;
-        JOptionPane.showMessageDialog(null,"Ha añadido: "+this.nCompletos+ " completos");
+    public int añadirCompletos(int a) {
+        this.nCompletos = a + this.nCompletos;
+        JOptionPane.showMessageDialog(null,"Ha añadido: "+a+ " completos");
         return this.nCompletos;
     }
+    public int quitarCompletos(int a) throws IOException {
+        Menu menu = new Menu();
+        if (this.nCompletos == 0){
+            JOptionPane.showMessageDialog(null, "No ha añadido nada aún");
+            menu.ventana2();
+        }
+            this.nCompletos = this.nCompletos - a;
+        JOptionPane.showMessageDialog(null, "Ha quitado: " + a + " completos");
+        return this.nCompletos;
+    }
+
+
+
+    //sopaipillas
 
     public int añadirSopaipillas(int nSopaipillas) {
         this.nSopaipillas = nSopaipillas + this.nSopaipillas;
         JOptionPane.showMessageDialog(null,"Ha añadido: "+this.nSopaipillas+ " Sopaipillas");
         return this.nSopaipillas;
     }
+    public int quitarSopaipillas(int b) throws IOException {
+        Menu menu = new Menu();
+        if (this.nSopaipillas == 0){
+            JOptionPane.showMessageDialog(null, "No ha añadido nada aún");
+            menu.ventana2();
+        }
+        this.nSopaipillas = this.nSopaipillas - b;
+        JOptionPane.showMessageDialog(null, "Ha quitado: " + b + " Sopaipillas");
+        return this.nSopaipillas;
+    }
+
+
+
+    //panes con queso
 
     public int añadirPanesConQueso(int nPanesConQueso) {
         this.nPanesConQueso = nPanesConQueso + this.nPanesConQueso;
-        JOptionPane.showMessageDialog(null,"Ha añadido: "+this.nPanesConQueso+ " Sopaipillas");
+        JOptionPane.showMessageDialog(null,"Ha añadido: "+this.nPanesConQueso+ " panes con queso");
         return this.nPanesConQueso;
     }
-
-    public void validar(int numero) {
+    public int quitarPanConQueso(int c) throws IOException {
         Menu menu = new Menu();
+        if (this.nPanesConQueso == 0){
+            JOptionPane.showMessageDialog(null, "No ha añadido nada aún");
+            menu.ventana2();
+        }
+        this.nPanesConQueso = this.nPanesConQueso - c;
+        JOptionPane.showMessageDialog(null, "Ha quitado: " + c + " panes con queso");
+        return this.nPanesConQueso;
     }
-
-
 
 }
