0b0ae1ca4912140d541fc1e87724bccdb81b2836
Felipe-Valdes <61599227+Felipe-Valdes@users.noreply.github.com>
2021-01-24 17:25:32 -0300

    Update Menu.java
    -Eliminadas algunas líneas de código sin función
    -Añadido método para quitar productos
    -Añadida opción para regresar a la ventana 1
    -Añadido método para imprimir boleta y terminar la ejecución del programa

--- Código/Menu.java
+++ Código/Menu.java
@@ -1,22 +1,19 @@
 import javax.swing.*;
 import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Menu {
 
-    Scanner teclado = new Scanner(System.in);
+    GestorDatos imprimir = new GestorDatos();
+    Metodo metodos = new Metodo();
 
-
-    public enum Tipo {AñadirCompletos, AñadirSopaipillas, AñadirPanConQueso, VerLista, Pagar}
+    public enum Tipo {AñadirCompletos, AñadirSopaipillas, AñadirPanConQueso, VerLista,
+    AñadirProductos, QuitarCompletos, QuitarSopaipillas, QuitarPanConQueso, Pagar}
 
     public void ventana1() throws IOException {
-        Metodo metodos = new Metodo();
-        GestorDatos imprimir = new GestorDatos();
 
-        Tipo[] opciones = {Tipo.AñadirCompletos, Tipo.AñadirSopaipillas, Tipo.AñadirPanConQueso, Tipo.VerLista, Tipo.Pagar};
+        Tipo[] opciones1 = {Tipo.AñadirCompletos, Tipo.AñadirSopaipillas, Tipo.AñadirPanConQueso, Tipo.VerLista};
         Tipo opcion = (Tipo) JOptionPane.showInputDialog(null, "Seleccione una opción",
-                "SimpleMenu", JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[4]);
+                "SimpleMenu", JOptionPane.INFORMATION_MESSAGE, null, opciones1, opciones1[3]);
         while (opcion != null) {
             switch (opcion) {
                 case AñadirCompletos:
@@ -27,52 +24,111 @@ public class Menu {
                     }catch (NumberFormatException e){
                         System.out.println("invalido");
                     }
-
                     break;
 
                 case AñadirSopaipillas:
-                    int b = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas sopaipillas quiere añadir?"));
-                    metodos.añadirSopaipillas(b);
+                    try {
+                        int b = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas sopaipillas quiere añadir?"));
+                        metodos.añadirSopaipillas(b);
+                    }catch (NumberFormatException e){
+                        System.out.println("invalido");
+                    }
                     break;
 
                 case AñadirPanConQueso:
-                    int c = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos panes con queso quiere añadir?"));
-                    metodos.añadirPanesConQueso(c);
+
+                    try {
+                        int c = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos panes con queso quiere añadir?"));
+                        metodos.añadirPanesConQueso(c);
+                    }catch (NumberFormatException e){
+                        System.out.println("invalido");
+                    }
                     break;
 
                 case VerLista:
                     ventana2();
                     break;
 
-                case Pagar:
-                    ventana3();
-                    imprimir.ImprimirBoleta();
-                    return;
-
-
-
             }
             opcion = (Tipo) JOptionPane.showInputDialog(null, "Seleccione una opcion",
-                    "SimpleMenu", JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[3]);
+                    "SimpleMenu", JOptionPane.INFORMATION_MESSAGE, null, opciones1, opciones1[3]);
         }
     }
 
+    public void ventana2() throws IOException {
+    Tipo[] opciones2 = {Tipo.AñadirProductos, Tipo.QuitarCompletos, Tipo.QuitarSopaipillas, Tipo.QuitarPanConQueso, Tipo.Pagar};
+    Tipo opcion = (Tipo) JOptionPane.showInputDialog(null, "Seleccione una opción",
+            "SimpleMenu", JOptionPane.INFORMATION_MESSAGE, null, opciones2, opciones2[4]);
+        while (opcion != null) {
+        switch (opcion) {
+            case AñadirProductos:
+                ventana1();
+                break;
 
+            case QuitarCompletos:
+                try{
+                    int a = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos completos quiere quitar?"));
+                    if (a==0) {
+                        JOptionPane.showMessageDialog(null, "No ha añadido completos aún");
+                        ventana2();
+                    }
+                    metodos.quitarCompletos(a);
+                }catch (NumberFormatException e){
+                    System.out.println("invalido");
+                }
+                break;
 
-    public void ventana2() {
-        JOptionPane.showMessageDialog(null, "Hasta ahora lleva:\n" +
+            case QuitarSopaipillas:
+                try{
+                    int b = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas sopaipillas quiere quitar?"));
+                    if (b==0) {
+                        JOptionPane.showMessageDialog(null, "No ha añadido sopaipillas aún");
+                        ventana2();
+                    }
+                    metodos.quitarSopaipillas(b);
+                }catch (NumberFormatException e){
+                    System.out.println("invalido");
+                }
+                break;
+
+            case QuitarPanConQueso:
+                try{
+                    int c = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos panes con queso quiere quitar?"));
+                    if (c==0) {
+                        JOptionPane.showMessageDialog(null, "No ha añadido panes con queso aún");
+                        ventana2();
+                    }
+                    metodos.quitarPanConQueso(c);
+                }catch (NumberFormatException e){
+                    System.out.println("invalido");
+                }
+                break;
+
+            case Pagar:
+                ventana3();
+                System.exit(0);
+
+        }
+        opcion = (Tipo) JOptionPane.showInputDialog(null, "Seleccione una opcion",
+                "SimpleMenu", JOptionPane.INFORMATION_MESSAGE, null, opciones2, opciones2[4]);
+    }
+}
+
+    /*JOptionPane.showMessageDialog(null, "Hasta ahora lleva:\n" +
                 +Metodo.nCompletos+" Completos\n" +
                 +Metodo.nSopaipillas+" Sopaipillas\n" +
-                +Metodo.nPanesConQueso+" Panes con queso");
-    }
+                +Metodo.nPanesConQueso+" Panes con queso");*/
 
-    public void ventana3() {
+    public void ventana3() throws IOException {
         JOptionPane.showMessageDialog(null, "Ha comprado:\n" +
                 +Metodo.nCompletos+" Completos\n" +
                 +Metodo.nSopaipillas+" Sopaipillas\n" +
                 +Metodo.nPanesConQueso+" Panes con queso");
+        imprimir.ImprimirBoleta();
+
 
     }
 }
+
 
 
