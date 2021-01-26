dd9971767e5a3821ef0fb3f5834e7ffefee0c221
MariaJoseReyes <64438037+MariaJoseReyes@users.noreply.github.com>
2021-01-26 01:53:33 -0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Metodo crearProducto = new Metodo();
         Menu menu = new Menu();
         JOptionPane.showMessageDialog(null, "Bienvenido a SimpleMenu");
         menu.Ventana1();
