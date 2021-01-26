bf70ace23e411ee85ff8a5c79367cb4ad62c42c9
Felipe-Valdes <61599227+Felipe-Valdes@users.noreply.github.com>
2021-01-26 00:51:00 -0300

    Add files via upload

---
+++ src/test/java/MetodoTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MetodoTest {
+    private Metodo metodo;
+
+    @BeforeEach
+    void setUp() { metodo = new Metodo();}
+
+    @AfterEach
+    void tearDown() { metodo = null; }
+
+    @Test
+    void añadirCompletos() {
+    }
+
+    @Test
+    void quitarCompletos() {
+    }
+
+    @Test
+    void añadirSopaipillas() {
+    }
+
+    @Test
+    void quitarSopaipillas() {
+    }
+
+    @Test
+    void añadirPanesConQueso() {
+    }
+
+    @Test
+    void quitarPanConQueso() {
+    }
+}
