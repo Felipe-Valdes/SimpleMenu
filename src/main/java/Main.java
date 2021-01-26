1651da7666e496e0249ddf71860f2cf372b19173
Felipe-Valdes <61599227+Felipe-Valdes@users.noreply.github.com>
2021-01-26 03:50:34 -0300

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Menu menu = new Menu();
+        JOptionPane.showMessageDialog(null, "Bienvenido a SimpleMenu");
+        menu.Ventana1();
+
+    }
+
+}
