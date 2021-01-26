d9722dff2a49859770a0cac4f6094bdcbe7c4e3a
MariaJoseReyes <64438037+MariaJoseReyes@users.noreply.github.com>
2021-01-26 02:26:08 -0300

    Update Editar.java

--- src/main/java/Editar.java
+++ src/main/java/Editar.java
@@ -1,1 +1,45 @@
+public class Editar{
+  //no se pa q es esto pero lo pongo iwal
+  public enum Tipo{ AñadirCompletos, AñadirSopaipillas, AñadirPanConQueso, QuitarCompletos, QuitarSopaipillas, QuitarPanConQueso}
+  
+  public int añadir(){
+  Tipo[] opcionesAñadir = {Tipo.AñadirCompletos, Tipo.AñadirSopaipillas, Tipo.AñadirPanConQueso};
+        Tipo opcion = (Tipo) JOptionPane.showInputDialog(null, "Seleccione una opción\n" +
+                        "Presione Cancel para volver",
+                "SimpleMenu", JOptionPane.INFORMATION_MESSAGE, null, opcionesAñadir, opcionesAñadir[2]);
+        while (opcion != null) {
+            switch (opcion) {
+                case AñadirCompletos:
+                    try{
+                        int a = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos completos quiere añadir?"));
+                        metodos.añadirCompletos(a);
+                    }catch (NumberFormatException e){
+                        System.out.println("invalido");
+                    }
+                    break;
 
+                case AñadirSopaipillas:
+                    try {
+                        int b = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas sopaipillas quiere añadir?"));
+                        metodos.añadirSopaipillas(b);
+                    }catch (NumberFormatException e){
+                        System.out.println("invalido");
+                    }
+                    break;
+
+                case AñadirPanConQueso:
+                    try {
+                        int c = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos panes con queso quiere añadir?"));
+                        metodos.añadirPanesConQueso(c);
+                    }catch (NumberFormatException e){
+                        System.out.println("invalido");
+                    }
+                    break;
+
+            }
+            opcion = (Tipo) JOptionPane.showInputDialog(null, "Seleccione una opcion\n" +
+                            "Pulse Cancel para volver",
+                    "SimpleMenu", JOptionPane.INFORMATION_MESSAGE, null, opcionesAñadir, opcionesAñadir[2]);
+        }
+}
+}
